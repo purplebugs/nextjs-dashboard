3121739d7226a97d078aab506697ef60f1e1a150
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-03 06:57:23 +0200

    style: Chapter 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+{
+  /*  import styles from '@/app/ui/home.module.css';*/
+  // https://nextjs.org/learn/dashboard-app/css-styling#css-modules
+}
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -10,6 +15,12 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          {/* // https://nextjs.org/learn/dashboard-app/css-styling#css-modules 
+          <div className={styles.shape} />
+          */}
+
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
